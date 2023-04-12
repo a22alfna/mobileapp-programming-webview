753ae8046ed49fed1321d0d33377cd270653d59d
a22alfna <a22alfna@student.his.se>
2023-04-12 12:25:32 +0200

    Jag implementerade min externa och interna webpage så att den öppnar respektive sida.

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -15,11 +15,11 @@ public class MainActivity extends AppCompatActivity {
     private WebView myWebView;
 
     public void showExternalWebPage(){
-        // TODO: Add your code for showing external web page here
+        myWebView.loadUrl("https://his.se");
     }
 
     public void showInternalWebPage(){
-        // TODO: Add your code for showing internal web page here
+        myWebView.loadUrl("file:///android_asset/main/Stuff.html");
     }
 
     @Override
